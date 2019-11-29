51239f50fb657455c0cdb1001140ac7b792f1ea6
Florient CHOUTEAU <florient.chouteau@gmail.com>
2019-11-29 22:10:14 +0100

    Custom index.html

--- teacher/build_docs_site.sh
+++ teacher/build_docs_site.sh
@@ -1,8 +1,10 @@
 rm -rf ../docs
 cd docs && reveal-md . --css static/style.css --static=../../docs --static-dirs=static --absolute-url https://fchouteau.github.io/isae-practical-gcp
 cp static/style.css ../../docs/css/style.css
+cp 0_index.html ../../docs/index.html
 sed -i 's|<link rel="stylesheet" href="/css/theme/black.css" id="theme" />|<link rel="stylesheet" href="/css/theme/white.css" />|g' ../../docs/index.html
 sed -i 's|color: white;|color: black;|g' ../../docs/index.html
+sed -i 's|<link rel="stylesheet" href="./_assets/static/style.css" />|<link rel="stylesheet" href="./css/style.css" />|g' ../../docs/index.html
 sed -i 's|<link rel="stylesheet" href="./_assets/static/style.css" />|<link rel="stylesheet" href="./css/style.css" />|g' ../../docs/0_intro.html
 sed -i 's|<link rel="stylesheet" href="./_assets/static/style.css" />|<link rel="stylesheet" href="./css/style.css" />|g' ../../docs/1_cloud.html
 sed -i 's|<link rel="stylesheet" href="./_assets/static/style.css" />|<link rel="stylesheet" href="./css/style.css" />|g' ../../docs/2_docker.html
